3f32bc307babbe74ba78edb11ea6d9a68dc570fb
Elvio Batista <elvio.batista@pebmed.com.br>
2022-08-08 18:08:34 -0300

    feat: adiciona testes com jest

---
+++ __test__/integration/image-analysis.test.js
@@ -0,0 +1,7 @@
+import { describe, test } from '@jest/globals';
+
+describe('Image analyser', () => {
+    test.todo('Deve analisar uma imagem com URL correta - status code 200');
+    test.todo('Deve dar erro ao receber um URL vazia - status code 400');
+    test.todo('Deve dar erro ao receber uma URL inválida - status code 500');
+})
